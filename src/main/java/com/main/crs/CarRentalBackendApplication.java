4412938be8e09db9e22ab442aa6df6011f704221
LivingLaser <dassayantan450@gmail.com>
2025-04-28 11:11:41 +0530

    Minor changes in Amdin Login

--- src/main/java/com/main/crs/CarRentalBackendApplication.java
+++ src/main/java/com/main/crs/CarRentalBackendApplication.java
@@ -9,13 +9,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.main.crs.config.AppConstants;
 import com.main.crs.entity.Role;
+import com.main.crs.entity.User;
 import com.main.crs.repository.RoleRepo;
+import com.main.crs.repository.UserRepo;
 
 @SpringBootApplication
 public class CarRentalBackendApplication implements CommandLineRunner {
 	
 	@Autowired
 	private RoleRepo roleRepo;
+	
+	@Autowired
+	private UserRepo userRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalBackendApplication.class, args);
@@ -28,6 +33,14 @@ public class CarRentalBackendApplication implements CommandLineRunner {
 		Role userRole = new Role(AppConstants.USER_NORMAL, "ROLE_USER");
 		List<Role> roles = List.of(adminRole, employeeRole, userRole);
 		roleRepo.saveAll(roles);
+		
+		User admin = new User();
+		admin.setUserId(1);
+		admin.setName("ADMIN");
+		admin.setEmail("admin@crs.com");
+		admin.setPassword("nimda");
+		admin.setRole(adminRole);
+		userRepo.save(admin);
 	}
 
 }
